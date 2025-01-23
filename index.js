cdb035d6b75bb9f65a67c100c7c3edeb4881cd13
alvaro <aalvarogalvez@gmail.com>
2025-01-23 12:26:21 -0500

    backend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const db = require('./initdb');
 const geoip = require('geoip-lite');
+const morgan = require('morgan');
+
 const PORT = 4000;
 const app = express();
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
     res.send("Hola mundo");
